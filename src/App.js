5d50b1e9025507ef8c56564e9558aadd429b953a
leezer94 <2kunhee94@gmail.com>
2022-02-26 19:40:27 +0900

    Feat: add constants

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Model } from './Model/model.js';
+import { Controller } from './Controller/controller.js';
+import { View } from './View/view.js';
+
+class App {
+  constructor() {
+    const view = new View();
+    const model = new Model();
+    const controller = new Controller(view, model);
+
+    controller.initCalender();
+  }
+}
+
+new App();
---
+++ src/common/constants.js
@@ -0,0 +1,19 @@
+export const POSITION = {
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+};
+
+export const STAGE = {
+  PREV: 'prev',
+  NEXT: 'next',
+};
+
+export const MONTH = {};
+
+export const NUMBER = {
+  ZERO: 0,
+  ONE: 1,
+  WEEKDAYS: 7,
+  DEC: 12,
+  JAN: 1,
+};
